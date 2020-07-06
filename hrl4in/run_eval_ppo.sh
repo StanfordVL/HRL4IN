cdf5af59f7363ec2f35866b18b544a8d45606cbb
veys <yveys@caltech.edu>
2020-07-06 12:33:07 -0700

    change scripts

--- hrl4in/run_eval_ppo.sh
+++ hrl4in/run_eval_ppo.sh
@@ -5,7 +5,7 @@ reward_type="dense"
 pos="random"
 lr="1e-4"
 num_steps="1024"
-run="jr_interactive_nav"
+run="jr2_reaching_2"
 
 log_dir="reward_"$reward_type"_pos_"$pos"_lr_"$lr"_nsteps_"$num_steps"_run_"$run
 echo $log_dir
@@ -30,7 +30,7 @@ python -u train_ppo.py \
    --experiment-folder "ckpt/"$log_dir \
    --checkpoint-interval 10 \
    --env-type "gibson" \
-   --config-file "jr_interactive_nav.yaml" \
+   --config-file "jr2_reaching.yaml" \
    --arena "stadium" \
    --num-eval-episodes 100 \
    --env-mode "gui" \
