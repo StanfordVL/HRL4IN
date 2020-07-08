e6b0c5ff13709fc2a9f506ee962a63952b018541
veys <yveys@caltech.edu>
2020-07-07 20:43:43 -0700

    Lower speed

--- hrl4in/run_eval_ppo.sh
+++ hrl4in/run_eval_ppo.sh
@@ -4,13 +4,14 @@ gpu="0"
 pos="rand_h"
 reward_type="l2"
 tol=0.1
-success_reward=100.0
+success_reward=20.0
 potential_reward=1.0
 gamma=0.99 
 lr="1e-4"
 num_steps="250"
+speed="0.025"
 
-log_dir="pos_"$pos"_rwd_"$reward_type"_tol_"$tol"_suc_rwd_"$success_reward"_pot_rwd_"$potential_reward"_gma_"$gamma"_lr_"$lr"_nstps_"$num_steps
+log_dir="pos_"$pos"_rwd_"$reward_type"_tol_"$tol"_suc_rwd_"$success_reward"_pot_rwd_"$potential_reward"_gma_"$gamma"_lr_"$lr"_nstps_"$num_steps"_spd_"$speed
 echo $log_dir
 
 python -u train_ppo.py \
