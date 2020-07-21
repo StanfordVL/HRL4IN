f1b093fae6812d73c93dbfed625146b20997f8c5
veys <yveys@caltech.edu>
2020-07-20 17:22:52 -0700

    Fixed with joint vel

--- hrl4in/run_eval_ppo.sh
+++ hrl4in/run_eval_ppo.sh
@@ -17,13 +17,13 @@
 gpu="0"
 pos="rdhp_sa_1.2"
 reward_type="l2"
-tol=0.2
+tol=0.05
 success_reward=10.0
 potential_reward=30.0
 col_reward=0.0
 gamma=0.99 
 lr="1e-4"
-num_steps="70"
+num_steps="140"
 speed="0.25_0.25"
 
 log_dir="pos_"$pos"_tol_"$tol"_suc_rwd_"$success_reward"_pot_rwd_"$potential_reward"_col_rwd_"$col_reward"_gma_"$gamma"_lr_"$lr"_nstps_"$num_steps"_spd_"$speed
--- hrl4in/run_train_ppo.sh
+++ hrl4in/run_train_ppo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 gpu="0"
-pos="rdhp_sa_1.2"
+pos="fix_s_1.2"
 reward_type="l2"
 tol=0.2
 success_reward=10.0
@@ -9,7 +9,7 @@ potential_reward=30.0
 col_reward=0.0
 gamma=0.99 
 lr="1e-4"
-num_steps="70"
+num_steps="60"
 speed="0.25_0.25"
 
 log_dir="pos_"$pos"_tol_"$tol"_suc_rwd_"$success_reward"_pot_rwd_"$potential_reward"_col_rwd_"$col_reward"_gma_"$gamma"_lr_"$lr"_nstps_"$num_steps"_spd_"$speed
@@ -39,5 +39,4 @@ python -u train_ppo.py \
    --arena "stadium" \
    --num-eval-episodes 1 \
    --env-mode "headless" \
-   --gamma $gamma \
-   --random-height
+   --gamma $gamma 
