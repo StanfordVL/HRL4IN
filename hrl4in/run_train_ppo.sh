4e31959f5384ec3da7cd1ae663d99e6395b757de
veys <yveys@caltech.edu>
2020-09-10 12:10:00 -0700

    just rgb images

--- hrl4in/run_train_ppo.sh
+++ hrl4in/run_train_ppo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 gpu="0"
-pos="head_2.5"
+pos="head"
 reward_type="l2"
 tol=0.5
 success_reward=30.0
@@ -9,8 +9,8 @@ potential_reward=5.0
 col_reward=-10.0
 gamma=0.99 
 lr="1e-4"
-num_steps="400"
-speed="0.2_0.2"
+num_steps="250"
+speed="0.1_0.05"
 
 log_dir="jr2_"$pos"_tol_"$tol"_suc_rwd_"$success_reward"_pot_rwd_"$potential_reward"_col_rwd_"$col_reward"_gma_"$gamma"_lr_"$lr"_nstps_"$num_steps"_spd_"$speed
 echo $log_dir
