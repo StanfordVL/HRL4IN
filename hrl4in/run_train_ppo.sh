023a7cc49ba06c7925a611157fd320242ec91255
veys <yveys@caltech.edu>
2020-09-26 17:20:45 -0700

    Obstacle test

--- hrl4in/run_train_ppo.sh
+++ hrl4in/run_train_ppo.sh
@@ -9,7 +9,7 @@ potential_reward=5.0
 col_reward=-5.0
 gamma=0.99
 lr="1e-4"
-num_steps="200"
+num_steps="250"
 speed="0.2_0"
 
 log_dir="jr2_"$pos"_tol_"$tol"_suc_rwd_"$success_reward"_pot_rwd_"$potential_reward"_col_rwd_"$col_reward"_gma_"$gamma"_lr_"$lr"_nstps_"$num_steps"_spd_"$speed
